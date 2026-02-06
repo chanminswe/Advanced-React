46a92c83c76fbb7b871d135d53db94576fdabf50
chanminswe <chanswe67@gmail.com>
2026-02-06 21:00:45 +0630

    feat : added conclusion for first part of chap-3

--- src/chapter-3/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactElement } from "react";
-
-function Button({icon} : { icon : ReactElement }) {
-  return (
-     <button className="button">Submit {icon}</button>
-  )
-}
-
-export default Button
--- src/chapter-3/configuration-concerns-with-elements-as-props/Main.tsx
+++ src/chapter-3/configuration-concerns-with-elements-as-props/Main.tsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import ErrorIcon from '@mui/icons-material/Error';
-import Loading from '@mui/icons-material/HourglassEmpty';
 import WarningIcon from '@mui/icons-material/Warning';
-import Button from '../components/Button';
+import type { ReactElement } from "react";
+
 
 
 type IconProps = {
   color: string;
   size?: 'large' | 'medium' | 'small';
 };
+
+function Button({icon} : { icon : ReactElement }) {
+  return (
+     <button className="button">Submit {icon}</button>
+  )
+}
+
 
 const Error = ({ color, size }: IconProps) => <ErrorIcon style={{ color }} fontSize={size} />;
 const Warning = ({ color, size }: IconProps) => <WarningIcon style={{ color }} fontSize={size} />;
